e68bdfadec4fe9ce2dd85d0a43278890ef455262
LevBernstein <lev2000@me.com>
2021-05-23 13:14:18 -0400

    switch to mod

--- conway.py
+++ conway.py
@@ -6,11 +6,11 @@ from time import sleep
 from os import name, system
 
 # Board size
-WIDTH = 30
+WIDTH = 60
 HEIGHT = 30
 
 # Number of generations to simulate
-RUNS = 200
+TICKS = 500
 
 def setup():
     system("cls" if name == 'nt' else "clear") # assume everything but Windows is unix-based
@@ -28,20 +28,20 @@ def status(board):
         answer = ""
         for num in row: answer += "O   " if num == 1 else "_   "
         print(answer[:-3] + "\n")
-    sleep(.25)
+    sleep(.2)
 
 def alive(board, y, x):
-    left = x - 1 if x > 0 else WIDTH-1
-    right = x + 1 if x < WIDTH-1 else 0
-    up = y - 1 if y > 0 else HEIGHT-1
-    down = y + 1 if y < HEIGHT-1 else 0
+    left = (x - 1) % WIDTH
+    right = (x + 1) % WIDTH
+    up = (y - 1) % HEIGHT
+    down = (y + 1) % HEIGHT
     neighbors = sum([board[up][left], board[y][left], board[down][left], board[up][x], board[down][x], board[up][right], board[y][right], board[down][right]])
     return 1 if (board[y][x] == 0 and neighbors == 3) or (board[y][x] == 1 and (neighbors == 2 or neighbors == 3)) else 0
 
 def game(board, generations):
     status(board)
-    for i in range(generations):
-        newBoard = [[0 for i in range(WIDTH)] for j in range(HEIGHT)]
+    for n in range(generations):
+        newBoard = [[0 for a in range(WIDTH)] for b in range(HEIGHT)]
         for i in range(HEIGHT):
             for j in range(WIDTH): newBoard[i][j] = alive(board, i, j)
         if board == newBoard:
@@ -50,6 +50,7 @@ def game(board, generations):
         status(board)
         if sum(row.count(1) for row in board) == 0:
             break
+    print("Ended on generation " + str(n+1))
 
 if __name__ == "__main__":
-    game(setup(), RUNS)
+    game(setup(), TICKS)
