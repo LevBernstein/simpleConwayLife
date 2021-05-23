ec85e368c9e6f98ab4c4c8d2598a3314301c0494
LevBernstein <lev2000@me.com>
2021-05-23 12:07:38 -0400

    long line fix

--- conway.py
+++ conway.py
@@ -5,14 +5,12 @@ from random import randint
 from time import sleep
 from os import name, system
 
-# MUSTFIX: sometimes random lines show up that should not exist
-
 # Board size
 WIDTH = 30
 HEIGHT = 30
 
 # Number of generations to simulate
-RUNS = 100
+RUNS = 200
 
 def setup():
     system("cls" if name == 'nt' else "clear") # assume everything but Windows is unix-based
@@ -25,8 +23,7 @@ def setup():
     return board
 
 def status(board):
-    #system("cls" if name == 'nt' else "clear") # assume everything but Windows is unix-based
-    print("AAAAAAAAAAAAAAAAA") # temporary while I try to figure out what's going wrong
+    system("cls" if name == 'nt' else "clear") # assume everything but Windows is unix-based
     for row in board:
         answer = ""
         for num in row: answer += "O   " if num == 1 else "_   "
@@ -34,14 +31,11 @@ def status(board):
     sleep(.25)
 
 def alive(board, y, x):
-    neighbors = 0
-    left = x - 1 if x > 0 else 9
-    right = x + 1 if x < 9 else 0
-    up = y - 1 if y > 0 else 9
-    down = y + 1 if y < 9 else 0
-    #print(left, right, up, down)
-    neighbors += board[up][left] + board[y][left] + board[down][left] + board[up][x] + board[down][x] + board[up][right] + board[y][right] + board[down][right]
-    #print(neighbors)
+    left = x - 1 if x > 0 else WIDTH-1
+    right = x + 1 if x < WIDTH-1 else 0
+    up = y - 1 if y > 0 else HEIGHT-1
+    down = y + 1 if y < HEIGHT-1 else 0
+    neighbors = sum([board[up][left], board[y][left], board[down][left], board[up][x], board[down][x], board[up][right], board[y][right], board[down][right]])
     return 1 if (board[y][x] == 0 and neighbors == 3) or (board[y][x] == 1 and (neighbors == 2 or neighbors == 3)) else 0
 
 def game(board, generations):
@@ -50,8 +44,12 @@ def game(board, generations):
         newBoard = [[0 for i in range(WIDTH)] for j in range(HEIGHT)]
         for i in range(HEIGHT):
             for j in range(WIDTH): newBoard[i][j] = alive(board, i, j)
+        if board == newBoard:
+            break
         board = newBoard
         status(board)
+        if sum(row.count(1) for row in board) == 0:
+            break
 
 if __name__ == "__main__":
     game(setup(), RUNS)
